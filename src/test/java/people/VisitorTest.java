103a1c1109501cca7263357668d9019043e66af5
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2023-01-24 07:14:21 +0000

    added visitor attraction logic and tests

--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,5 +1,7 @@
 package people;
 
+import attractions.Dodgems;
+import attractions.RollerCoaster;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,10 +10,15 @@ import static org.junit.Assert.assertEquals;
 public class VisitorTest {
 
     Visitor visitor;
+    RollerCoaster rollerCoaster;
+    Dodgems dodgems;
 
     @Before
     public void before(){
         visitor = new Visitor(14, 1.2, 40.0);
+        rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        dodgems = new Dodgems("Bumper Cars", 5);
+        visitor.addVisitedAttraction(rollerCoaster);
     }
 
     @Test
@@ -27,5 +34,16 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void hasVisitedAttractions() {
+        assertEquals(1, visitor.getVisitedAttractions().size());
+    }
+
+    @Test
+    public void canAddVisitedAttractions() {
+        visitor.addVisitedAttraction(dodgems);
+        assertEquals(2, visitor.getVisitedAttractions().size());
     }
 }
